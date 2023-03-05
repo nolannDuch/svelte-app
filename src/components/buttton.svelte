d53f2a9a0e7ac6ad03ca53999533ed47a371ce2e
Dell <nolann.duchaussoy@gmail.com>
2023-03-05 16:34:10 +0100

    first commit

---
+++ src/components/buttton.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    export let color: string;
+	let clicked = true;
+
+    function handleClick (): void {
+		clicked = !clicked;
+	}
+</script>
+
+<div>
+    <button style="color:{color};" on:click={handleClick}>click</button>
+    <br/>
+    etat du click : {clicked}
+</div>
+
+<style>
+
+</style>
